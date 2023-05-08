5b9ce3653cad0f8c190da2e625c51f3d43945b01
Zhiming Xiong <zxiong@redhat.com>
2023-05-08 10:22:48 +0800

    fix coverage

--- pubtools/_sign/signers.py
+++ pubtools/_sign/signers.py
@@ -1,6 +1,12 @@
 class Signer:
-
     """Class for signing."""
 
-    def load_config(config_data: dict[str, any]):
+    def load_config(self, config_data: dict[str, any]):
+        """
+        Load configuration.
+
+        Args:
+            config_data (dict[str, any]):
+                dictionary of configuration data.
+        """
         raise NotImplementedError
